37bb036ec49f20fc5602e88fc113cb291a902ea2
Darkmen203 <dima-pivovarov03@mail.ru>
2024-05-24 20:20:25 +0300

    added new params for auth github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,45 +8,74 @@ class SessionsController < ApplicationController
     end
    
     def github_callback
-     client_id = ENV['GITHUB_ID']
-     client_secret = ENV['GITHUB_SECRET']
-     code = params[:code]
-   
-     response = HTTParty.post('https://github.com/login/oauth/access_token', {
-                                body: {
-                                  client_id: client_id,
-                                  client_secret: client_secret,
-                                  code: code
-                                },
-                                mode: 'no-cors',
-       headers: { 'Accept' => 'application/json' }
-                              })
-   
-     access_token = response.parsed_response['access_token']
-   
-     # Используйте access_token для получения информации о пользователе
-     user_info = HTTParty.get('https://api.github.com/user', {
-                                headers: {
-                                  'Authorization' => "token #{access_token}",
-                                  'User-Agent' => 'RailsApp'
-                                },
-                                mode: 'no-cors',
-                              })
-     user_data = user_info.parsed_response
-     puts user_data
-     user = User.find_or_create_by(id: user_data['id'], provider: 'github') do |u|
-      u.login = user_data['login']
-      u.email = user_data['email']
-      u.token = access_token
-     end
-     puts user.name
-     puts user.email
-     puts user.token
-     puts user.provider
-     puts user.id
-     session[:user_id] = user.id
-   
-     redirect_to root_path, notice: 'Signed in!'
+      client_id = ENV['GITHUB_ID']
+      client_secret = ENV['GITHUB_SECRET']
+      code = params[:code]
+    
+      response = HTTParty.post('https://github.com/login/oauth/access_token', {
+                                  body: {
+                                    client_id: client_id,
+                                    client_secret: client_secret,
+                                    code: code
+                                  },
+                                  mode: 'no-cors',
+        headers: { 'Accept' => 'application/json' }
+                                })
+    
+      access_token = response.parsed_response['access_token']
+    
+      # Используйте access_token для получения информации о пользователе
+      user_info = HTTParty.get('https://api.github.com/user', {
+                                  headers: {
+                                    'Authorization' => "token #{access_token}",
+                                    'User-Agent' => 'RailsApp'
+                                  },
+                                })
+      user_data = user_info.parsed_response
+      puts user_data
+
+
+       # Получение email адресов
+      email_info = HTTParty.get('https://api.github.com/user/emails', {
+        headers: {
+          'Authorization' => "token #{access_token}",
+          'User-Agent' => 'RailsApp'
+        }
+      })
+
+      if email_info.parsed_response.is_a?(Hash) && email_info.parsed_response['message']
+        redirect_to root_path, alert: 'Ошибка при получении email адреса пользователя с GitHub.'
+        return
+      end
+
+      emails = email_info.parsed_response
+      primary_email = emails.find { |email| email['primary'] && email['verified'] }&.dig('email')
+      primary_email ||= emails.find { |email| email['verified'] }&.dig('email')
+
+      if primary_email.nil?
+        redirect_to root_path, alert: 'Не удалось получить email адрес пользователя с GitHub.'
+        return
+      end
+
+      user = User.find_or_create_by(id: user_data['id'], provider: 'github') do |u|
+        u.login = user_data['login']
+        u.email = primary_email
+        u.name = user_data['name'] ? user_data['name'] : user_data['login']
+        u.avatar_url = user_data['avatar_url']
+        u.token = access_token
+      end
+      puts 'user.login'
+      puts user.login
+      puts 'user.name'
+      puts user.name
+      puts 'user.email'
+      puts user.email
+      puts user.token
+      puts user.provider
+      puts user.id
+      session[:user_id] = user.id
+    
+      redirect_to root_path, notice: 'Signed in!'
     end
    
     def destroy
