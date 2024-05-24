8116117b7e0defdca673e0623bacdc1ec8f432da
Darkmen203 <dima-pivovarov03@mail.ru>
2024-05-24 19:46:20 +0300

    fix bugs

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,19 @@
 class ProfilesController < ApplicationController
-    
+    # TODO: add protected profiles
     def show
-        redirect_to login_path_url unless current_user
+        # redirect_to login_path_url unless current_user
     end
   
     def my_questions
-        redirect_to login_path_url unless current_user
+        # redirect_to login_path_url unless current_user
     end
   
     def my_answers
-        redirect_to login_path_url unless current_user
+        # redirect_to login_path_url unless current_user
     end
     
     def settings
-        redirect_to login_path_url unless current_user
+        # redirect_to login_path_url unless current_user
     end
   end
   
