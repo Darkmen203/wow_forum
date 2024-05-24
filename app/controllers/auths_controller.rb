4c3e8efee94dbd454d70ad14da8912055b81c5e7
skvoznyak3 <grisha_grigorev_02@mail.ru>
2024-05-25 01:54:37 +0300

    created sign in/sign up

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,12 +1,40 @@
 class AuthsController < ApplicationController
-    def login
-    #   @questions = Question.all.order(:created_at)
-      redirect_to root_path, notice: "You are logining" unless !current_user
-    end
-  
-    def register
-    #   @question = Question.find(params[:id])
-      redirect_to root_path, notice: "You are logining" unless !current_user
+
+    def create
+      puts params.inspect
+      if params[:password] == params[:password_confirmation] and User.find_by_login(params[:login]).nil?
+        u = User.create(
+          provider: 'wowforum',
+          password: params[:password],
+          login: params[:login],
+          email: params[:email],
+          name: params[:login],
+          avatar_url: '/skull-solid.svg',
+          token: rand(1..1000))
+
+        session[:user_id] = u.id
+        session[:name] = u.name
+        session[:avatar_url] = u.avatar_url
+        redirect_to root_path
+      else
+        redirect_to register_path_url
+      end
 
     end
+
+    def sign
+      puts params.inspect
+      u = User.find_by(password: params[:password], email: params[:email])
+      puts 'sss', u.inspect
+      puts 'sssssssssssssss',u.id
+
+      if not u.nil?
+        session[:user_id] = u.id
+        session[:avatar_url] = u.avatar_url
+        session[:name] = u.name
+        redirect_to root_path, notice: "You are logining"
+      else redirect_to login_path_url
+      end
+    end
+
   end
