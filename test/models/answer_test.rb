9bfb635ad2808d93f4b40c94c0a9ae3529c9793f
Baklachok <korobko_01@inbox.ru>
2024-05-25 21:56:56 +0300

    tests for models

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,7 +1,29 @@
-require "test_helper"
+require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user_one)
+    @question = questions(:question_one)
+    @answer = Answer.new(body: "Test answer", user: @user, question: @question)
+  end
+
+  test "should be valid" do
+    assert @answer.valid?
+  end
+
+  test "body should be present" do
+    @answer.body = "   "
+    assert_not @answer.valid?
+  end
+
+  test "user should be present" do
+    @answer.user = nil
+    assert_not @answer.valid?
+  end
+
+  test "question should be present" do
+    @answer.question = nil
+    assert_not @answer.valid?
+  end
 end
+
